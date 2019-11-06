54dd59d47dfef075d7ba07c37db7dd26a43a94d6
Manny <manueljosetorres02@gmail.com>
2019-11-05 22:19:37 -0600

    Set up HOC Portion

--- react-complete-guide/Module 7-2_HOC/cmp-deep-dive-07-withclass/src/components/Persons/Person/Person.js
+++ react-complete-guide/Module 7-2_HOC/cmp-deep-dive-07-withclass/src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 class Person extends Component {
